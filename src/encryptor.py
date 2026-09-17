4f250d59ecb2f742ea954fe62d88be971a5014d4
Gonçalo_Rodrigues <G.dRCPRO@protonmail.com>
2026-09-17 10:30:26 +0100

    Add Caesar cipher Encryptor

--- src/encryptor.py
+++ src/encryptor.py
@@ -1,2 +1,18 @@
+
 class Encryptor:
+    """Provides a basic Caesar Cypher encryptor. Skips non-alphabetic characters and lowers the letters"""
+
     def __init__(self):
+        self.alphabetNumber = 26
+
+    def encrypt(self, message: str, key: int):
+        result = ""
+
+        for letter in message:
+            letter = letter.lower()
+            if letter.isalpha():
+                result += chr(((ord(letter) - ord("a") + key) % self.alphabetNumber) + ord("a"))
+            else:
+                result += letter
+
+        return result
